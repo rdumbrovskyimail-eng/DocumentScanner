8d258f14f6b66392a34a6dfa77b58b80dce2b2e0
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2025-12-14 00:18:23 +0100

    Add files via upload

---
+++ app/src/main/java/com/docs/scanner/presentation/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.docs.scanner.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.docs.scanner.presentation.navigation.NavGraph
+import com.docs.scanner.presentation.theme.DocumentScannerTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        enableEdgeToEdge()
+        
+        setContent {
+            DocumentScannerTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    NavGraph()
+                }
+            }
+        }
+    }
+}
