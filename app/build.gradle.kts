2b84403bf1e461e538b01ffada01ca98281bc560
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2025-12-14 00:56:52 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.docs.scanner"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.docs.scanner"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
