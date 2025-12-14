a67bce8db9a5ef5ebc985adf625e74e5fc3935ba
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2025-12-14 15:29:15 +0100

    Add files via upload

--- app/src/main/java/com/docs/scanner/data/local/database/AppDatabase.kt
+++ app/src/main/java/com/docs/scanner/data/local/database/AppDatabase.kt
@@ -1,29 +1,24 @@
 package com.docs.scanner.data.local.database
-
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.docs.scanner.data.local.database.dao.DocumentDao
-import com.docs.scanner.data.local.database.dao.FolderDao
-import com.docs.scanner.data.local.database.dao.RecordDao
-import com.docs.scanner.data.local.database.entities.DocumentEntity
-import com.docs.scanner.data.local.database.entities.FolderEntity
-import com.docs.scanner.data.local.database.entities.RecordEntity
-
+import com.docs.scanner.data.local.database.dao.*
+import com.docs.scanner.data.local.database.entities.*
 @Database(
-    entities = [
-        FolderEntity::class,
-        RecordEntity::class,
-        DocumentEntity::class
-    ],
-    version = 1,
-    exportSchema = false
+entities = [
+FolderEntity::class,
+RecordEntity::class,
+DocumentEntity::class,
+TermEntity::class
+],
+version = 2,
+exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
-    abstract fun folderDao(): FolderDao
-    abstract fun recordDao(): RecordDao
-    abstract fun documentDao(): DocumentDao
-    
-    companion object {
-        const val DATABASE_NAME = "document_scanner.db"
-    }
+abstract fun folderDao(): FolderDao
+abstract fun recordDao(): RecordDao
+abstract fun documentDao(): DocumentDao
+abstract fun termDao(): TermDao
+companion object {
+    const val DATABASE_NAME = "document_scanner.db"
+}
 }
