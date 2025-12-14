2ea4d094ec1a14e73488734830805622b119dc74
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2025-12-14 17:17:07 +0100

    Update FullscreenTextEditor.kt

--- app/src/main/java/com/docs/scanner/presentation/components/FullscreenTextEditor.kt
+++ app/src/main/java/com/docs/scanner/presentation/components/FullscreenTextEditor.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 
