0a57fc924c67dfe4b4be2f4fc24621ed8a1e2dac
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2025-12-14 01:50:52 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,10 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/INDEX.LIST"
+            excludes += "META-INF/*.SF"
+            excludes += "META-INF/*.DSA"
+            excludes += "META-INF/*.RSA"
         }
     }
 }
