14fa3d328174ceb01d085c7eb353592659d5d664
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2025-12-26 14:49:57 +0100

    Update GeminiApiService.kt

--- app/src/main/java/com/docs/scanner/data/remote/gemini/GeminiApiService.kt
+++ app/src/main/java/com/docs/scanner/data/remote/gemini/GeminiApiService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface GeminiApiService {
-    @POST("v1beta/models/gemini-2.0-flash-exp:generateContent")
+    @POST("v1beta/models/gemini-2.5-flash-latest:generateContent")
     suspend fun generateContent(
         @Query("key") apiKey: String,
         @Body request: GeminiRequest
