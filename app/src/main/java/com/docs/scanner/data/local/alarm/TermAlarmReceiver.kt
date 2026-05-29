a9559b4d5682df550eaf693b8b3743b0921d695c
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-05-29 19:40:07 +0200

    [Pipeline] TermAlarmReceiver.kt

--- app/src/main/java/com/docs/scanner/data/local/alarm/TermAlarmReceiver.kt
+++ app/src/main/java/com/docs/scanner/data/local/alarm/TermAlarmReceiver.kt
@@ -6,6 +6,7 @@ import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.media.RingtoneManager
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
