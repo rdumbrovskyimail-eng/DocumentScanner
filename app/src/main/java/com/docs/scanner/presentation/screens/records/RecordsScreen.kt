fd8ac49e97de02f821dacc20b1264c80fe4e1493
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-05-31 00:43:54 +0200

    [Pipeline] RecordsScreen.kt

--- app/src/main/java/com/docs/scanner/presentation/screens/records/RecordsScreen.kt
+++ app/src/main/java/com/docs/scanner/presentation/screens/records/RecordsScreen.kt
@@ -2,6 +2,8 @@ package com.docs.scanner.presentation.screens.records
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
